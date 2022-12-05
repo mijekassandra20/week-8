a91f877838838b4ef171321bd95c186a1d773772
mijekassandra20 <kassandra.mije@supportzebra.com>
2022-12-05 13:55:35 +0800

    Assignment 3: Week 8

--- validCreditCard.js
+++ validCreditCard.js
@@ -1,15 +1,12 @@
-const validCreditCard = (num) => {
-
-    let convert = String(num)
+const validCreditCard = (str) => {
     
     let regex = /^[4,5,6]+(\d{3}-?\d{4}-?\d{4}-?\d{4})/gm
     let regRepChar = /([\d])\1\1\1/gm
-    let replace = convert.replace(/-/g, '')
+    let replace = str.replace(/-/g, '')
     
-    let result = convert.match(regex)
+    let result = str.match(regex)
     let result2 = replace.match(regRepChar)
     
-    console.log(replace)
     
     if(result && !result2){
         return 'valid'
@@ -19,9 +16,9 @@ const validCreditCard = (num) => {
     
 }
 
-console.log(validCreditCard(4123456789123456)) // valid
+console.log(validCreditCard(`4123456789123456`)) // valid
 console.log(validCreditCard(`5123-4567-8912-3456`)) //valid
-console.log(validCreditCard(61234-567-8912-3456)) //invalid
-console.log(validCreditCard(4123356789123456)) // valid
-console.log(validCreditCard(5133-3367-8912-3456)) // invalid
-console.log(validCreditCard(5123 - 4567 - 8912 - 3456)) //invalid
+console.log(validCreditCard(`61234-567-8912-3456`)) //invalid
+console.log(validCreditCard(`4123356789123456`)) // valid
+console.log(validCreditCard(`5133-3367-8912-3456`)) // invalid
+console.log(validCreditCard(`5123 - 4567 - 8912 - 3456`)) //invalid
